c86977ef5f26af1cfcbae6fdfdf82596b502d9ff
Michael Salinger <mjsalinger@gmail.com>
2014-04-02 08:50:20 -0400

    Modified error message to reflect rooms rather than resources

--- business-logic/collections/bookings/onPreSave.js
+++ business-logic/collections/bookings/onPreSave.js
@@ -1,3 +1,19 @@
+/**
+ * Copyright (c) 2014 Kinvey, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 function onPreSave(request,response,modules){
   var rooms = modules.collectionAccess.collection('conferencerooms');
 
@@ -7,9 +23,9 @@ function onPreSave(request,response,modules){
       // Database query error, return an error response to the client and terminate the request
       response.error(err);
     } else {
-      // If no rooms found, End the request without saving, with a message of "no resource available"
+      // If no rooms found, End the request without saving, with a message of "no rooms available"
       if (!result._id) {
-        response.body = {message: "No Resources Available"};
+        response.body = {message: "No Rooms Available"};
         response.complete();  // completes the request and returns the result to the client 
       } else {
         // If a room was found, add the room to the temp object store, 
